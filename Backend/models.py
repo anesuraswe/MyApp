c52b21bbb452dfcf17e019ffbb5af94a972657c7
anesuraswe <147626052+anesuraswe@users.noreply.github.com>
2023-11-29 10:39:12 +0200

    Update models.py

--- Backend/models.py
+++ Backend/models.py
@@ -1,39 +1,106 @@
 from django.db import models
 
-class Admin(models.Model):
-    AdminName = models.CharField(max_length=100, db_column='AdminName')
-    AdminEmail = models.EmailField(unique=True, db_column='AdminEmail')
-    AdminPassword = models.CharField(max_length=100, db_column='AdminPassword')
+class PlantManager(models.Model):
+    ManagerID = models.AutoField(primary_key=True, db_column='ManagerID')
+    ManagerName = models.CharField(max_length=100, db_column='ManagerName')
+    ManagerSurname = models.CharField(max_length=100, db_column='ManagerSurname')
+    ManagerPhone = models.CharField(max_length=10, db_column='ManagerPhone')
+    ManagerEmail = models.EmailField(unique=True, db_column='ManagerEmail')
+    ManagerPassword = models.CharField(max_length=100, db_column='ManagerPassword')
+    ManagerFingerprint = models.BiometricField()
 
     def __str__(self):
-        return self.AdminName
+        return self.ManagerSurname
 
     class Meta:
-        db_table = 'admin'
+        db_table = 'manager'
 
 
-class User(models.Model):
-    UserID = models.AutoField(primary_key=True, db_column='UserID')
-    UserName = models.CharField(max_length=100, db_column='UserName')
-    UserTitle = models.CharField(max_length=100, db_column='UserTitle')
-    UserEmail = models.EmailField(unique=True, db_column='UserEmail')
-    UserPassword = models.CharField(max_length=100, db_column='UserPassword')
+
+class PlantClerk(models.Model):
+    ClerkID = models.AutoField(primary_key=True, db_column='ClerkID')
+    ClerkName = models.CharField(max_length=100, db_column='ClerkName')
+    ClerkSurname = models.CharField(max_length=100, db_column='ClerkSurname')
+    ClerkPhone = models.CharField(max_length=10, db_column='ClerkPhone')
+    ClerkEmail = models.EmailField(unique=True, db_column='ClerkEmail')
+    ClerkPassword = models.CharField(max_length=100, db_column='ClerkPassword')
+    ClerkFingerprint = models.BiometricField()
 
     def __str__(self):
-        return self.UserName
+        return self.ClerkSurname + self.ClerkName
 
     class Meta:
-        db_table = 'users'
+        db_table = 'clerk'
 
-class Laptop(models.Model):
-    SerialNumber = models.CharField(max_length=100, db_column='SerialNumber')
-    Model = models.CharField(max_length=100, db_column='Model')
-    Make = models.CharField(max_length=100, db_column='Make')
-    Color = models.CharField(max_length=100, db_column='Color')
-    Status = models.CharField(max_length=100, db_column='Status')
 
+
+
+class Engineer(models.Model):
+    EngineerID = models.AutoField(primary_key=True, db_column='EngineerID')
+    EngineerName = models.CharField(max_length=100, db_column='EngineerName')
+    EngineerSurname = models.CharField(max_length=100, db_column='EngineerSurname')
+    EngineerPhone = models.CharField(max_length=10, db_column='EngineerPhone')
+    EngineerEmail = models.EmailField(unique=True, db_column='EngineerEmail')
+    EngineerPassword = models.CharField(max_length=100, db_column='EngineerPassword')
+   
     def __str__(self):
-        return self.SerialNumber
+        return self.EngineerSurname
 
     class Meta:
-        db_table = 'laptops'
+        db_table = 'engineer'
+        
+
+class Operator(models.Model):
+  OperatorID = models.AutoField(primary_key=True, db_column='OperatorID')
+    OperatorName = models.CharField(max_length=100, db_column='OperatorName')
+    OperatorSurname = models.CharField(max_length=100, db_column='OperatorSurname')
+    Role = models.CharField(max_length=100, db_column='Role')
+      
+    def __str__(self):
+        return self.OperatorSurname + self.Role
+
+    class Meta:
+        db_table = 'operator'
+
+
+
+class Site(models.Model):
+    SiteID = models.AutoField(primary_key=True, db_column='SiteID')
+    SiteName = models.CharField(max_length=100, db_column='SiteName')
+       
+    def __str__(self):
+        return self.SiteName
+
+    class Meta:
+        db_table = 'site'
+
+
+
+
+class Plant(models.Model):
+    FleetCode = models.CharField(max_length=8, db_column='FleetCode')
+    PlantName = models.CharField(max_length=100, db_column='PlantName')
+    Make = models.CharField(max_length=30, db_column='Make')
+    RegNumber = models.CharField(max_length=10, unique=True, db_column='RegNumber')
+    ChaseNumber = models.CharField(max_length=10, db_column='ChaseNumber')
+    SiteID = models.ForeignKey(Site, on_delete=models.CASCADE)
+   
+    def __str__(self):
+        return self.FleetCode + self.PlantName
+
+    class Meta:
+        db_table = 'plant'
+
+
+class PlantStatus(models.Model):
+    PlantStatusCode = models.IntegerField(max_length=1, db_column='PlantStatusCode')
+    PlantStatusName = models.CharField(max_length=50, db_column='PlantStatusName')
+    FleetCode = models.ForeignKeiy(Plant, on_delete=models.CASCADE)
+    Description = models.CharField(max_length=100, db_column='Description')
+   
+    def __str__(self):
+        return self.FleetCode + self.PlantStatusName
+
+    class Meta:
+        db_table = 'plantstatus'
+
